0683820c37e8d5b2d4edaddff64fd17d769a9dc2
unknown <ramees639@gmail.com>
2023-07-23 13:15:53 +0530

    remove

--- demo_project/demo_app/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.index, name= 'home'),
-    # path('add/',views.addition,name='addition'),
-    # path('contact/',views.contact,name='contact')
-]
